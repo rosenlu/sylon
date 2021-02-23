1851552796e365b51c33ea27fe87d9ce70c2dda2
Luis Rosendahl <luisrosendahl@gmail.com>
2021-02-23 01:08:51 +0100

    Small example code

--- app/src/main/java/net/luisr/sylon/MainActivity.java
+++ app/src/main/java/net/luisr/sylon/MainActivity.java
@@ -3,6 +3,7 @@ package net.luisr.sylon;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btn).setOnClickListener((v -> Toast.makeText(this, R.string.test_message, Toast.LENGTH_LONG).show()));
     }
 }
