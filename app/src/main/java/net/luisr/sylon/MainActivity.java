59d66f14c5051a9d919256cc332665d42799be2b
Luis Rosendahl <luisrosendahl@gmail.com>
2021-03-01 22:57:06 +0100

    Implement preview

--- app/src/main/java/net/luisr/sylon/MainActivity.java
+++ app/src/main/java/net/luisr/sylon/MainActivity.java
@@ -1,17 +1,115 @@
 package net.luisr.sylon;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.Camera;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+import androidx.core.content.ContextCompat;
+import androidx.lifecycle.LifecycleOwner;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import com.google.common.util.concurrent.ListenableFuture;
+
+import java.io.File;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CODE_CAMERA = 10;
+
+    private ImageCapture imageCapture = null;
+
+    private File outputDirectory;
+    private ExecutorService cameraExecutor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.btn).setOnClickListener((v -> Toast.makeText(this, R.string.test_message, Toast.LENGTH_LONG).show()));
+        if (allPermissionsGranted()) {
+            startCamera();
+        } else {
+            requestPermissions(new String[] { Manifest.permission.CAMERA }, REQUEST_CODE_CAMERA);
+        }
+
+        findViewById(R.id.camera_capture_button).setOnClickListener(v -> takePhoto());
+
+        outputDirectory = getOutputDirectory();
+
+        cameraExecutor = Executors.newSingleThreadExecutor();
+    }
+
+    private File getOutputDirectory() {
+        File[] externalMediaDirs = getExternalMediaDirs();
+        File filesDir = getFilesDir();
+        if (externalMediaDirs.length != 0) {
+            File firstExtDir = externalMediaDirs[0];
+            if (firstExtDir != null && firstExtDir.exists()) {
+                File subDir = new File(firstExtDir, getString(R.string.app_name));
+                if (subDir.mkdirs()) {
+                    return subDir;
+                }
+            }
+        }
+        return filesDir;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_CODE_CAMERA) {
+            if (allPermissionsGranted()) {
+                startCamera();
+            } else {
+                Toast.makeText(this, R.string.permissions_denied, Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void takePhoto() {
+
+    }
+
+
+
+    private void startCamera() {
+        ListenableFuture<ProcessCameraProvider> cameraProviderFuture = ProcessCameraProvider.getInstance(this);
+
+        cameraProviderFuture.addListener(() -> {
+
+            try {
+                ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
+                Preview preview = new Preview.Builder().build();
+
+                CameraSelector cameraSelector = new CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK).build();
+
+                PreviewView previewView = findViewById(R.id.viewFinder);
+                preview.setSurfaceProvider(previewView.getSurfaceProvider());
+
+                //cameraProvider.unbindAll();
+                Camera camera = cameraProvider.bindToLifecycle(this, cameraSelector, preview);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }, ContextCompat.getMainExecutor(this));
+    }
+
+    private boolean allPermissionsGranted() {
+        return checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        cameraExecutor.shutdown();
     }
 }
