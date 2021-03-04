92cb29153f18b7a1983247c1031bbcaeb92751c4
Andreas Strasser <strassan@kaifeck.de>
2021-03-04 20:14:12 +0100

    Add functionality to the Camera Capture Button
    Images are named by the date and time when they were created and are stored in the subfolder Sylon/img/

--- app/src/main/java/net/luisr/sylon/MainActivity.java
+++ app/src/main/java/net/luisr/sylon/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.camera.core.Camera;
 import androidx.camera.core.CameraSelector;
 import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageCaptureException;
 import androidx.camera.core.Preview;
 import androidx.camera.lifecycle.ProcessCameraProvider;
 import androidx.camera.view.PreviewView;
@@ -13,28 +14,41 @@ import androidx.lifecycle.LifecycleOwner;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
 import com.google.common.util.concurrent.ListenableFuture;
 
 import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "Sylon";
 
     private static final int REQUEST_CODE_CAMERA = 10;
 
+    private final String FILENAME_FORMAT = "yyyy-MM-dd_HHmmss-SSS";
+
     private ImageCapture imageCapture = null;
 
-    private File outputDirectory;
+    private File imgDirectory;
+    private File pdfDirectory;
     private ExecutorService cameraExecutor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageCapture = new ImageCapture.Builder()
+                .setTargetRotation(this.getWindowManager().getDefaultDisplay().getRotation())
+                .build();
 
         if (allPermissionsGranted()) {
             startCamera();
@@ -44,7 +58,14 @@ public class MainActivity extends AppCompatActivity {
 
         findViewById(R.id.camera_capture_button).setOnClickListener(v -> takePhoto());
 
-        outputDirectory = getOutputDirectory();
+        File outputDirectory = getOutputDirectory();
+        imgDirectory = new File(outputDirectory, "img");
+        pdfDirectory = new File(outputDirectory, "pdf");
+        if (!imgDirectory.exists() || !pdfDirectory.exists()) {
+            if (!imgDirectory.mkdirs() || !pdfDirectory.mkdirs()) {
+                Log.w(TAG, "Could not create subdirs.");
+            }
+        }
 
         cameraExecutor = Executors.newSingleThreadExecutor();
     }
@@ -76,10 +97,24 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void takePhoto() {
+        File outputFile = new File(imgDirectory, new SimpleDateFormat(FILENAME_FORMAT, Locale.US).format(System.currentTimeMillis()) + ".jpg");
+        ImageCapture.OutputFileOptions outputFileOptions = new ImageCapture.OutputFileOptions.Builder(outputFile).build();
+        imageCapture.takePicture(outputFileOptions, cameraExecutor, new ImageCapture.OnImageSavedCallback() {
+            @Override
+            public void onImageSaved(@NonNull ImageCapture.OutputFileResults outputFileResults) {
+                String savedUri = Uri.fromFile(outputFile).toString();
+                String msg = "Image " + savedUri + " saved successfully!";
+                runOnUiThread(() -> Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show());
+                Log.d(TAG, msg);
+            }
 
+            @Override
+            public void onError(@NonNull ImageCaptureException exception) {
+//                Toast.makeText(MainActivity.this, "Image " + outputFile.toString() + " could not be saved", Toast.LENGTH_SHORT).show();
+                Log.w(TAG, "Image could not be saved: " + exception.getMessage());
+            }
+        });
     }
-
-
 
     private void startCamera() {
         ListenableFuture<ProcessCameraProvider> cameraProviderFuture = ProcessCameraProvider.getInstance(this);
@@ -96,7 +131,7 @@ public class MainActivity extends AppCompatActivity {
                 preview.setSurfaceProvider(previewView.getSurfaceProvider());
 
                 //cameraProvider.unbindAll();
-                Camera camera = cameraProvider.bindToLifecycle(this, cameraSelector, preview);
+                Camera camera = cameraProvider.bindToLifecycle(this, cameraSelector, imageCapture, preview);
             } catch (Exception e) {
                 e.printStackTrace();
             }
