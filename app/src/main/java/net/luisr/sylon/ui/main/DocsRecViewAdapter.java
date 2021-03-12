a94e238c5e8fa893a5e0abb4af146abb044b9507
Andreas Strasser <strassan@kaifeck.de>
2021-03-12 18:11:20 +0100

    Closes #5: Add documentation to the ui.main package

--- app/src/main/java/net/luisr/sylon/ui/main/DocsRecViewAdapter.java
+++ app/src/main/java/net/luisr/sylon/ui/main/DocsRecViewAdapter.java
@@ -5,6 +5,7 @@ import android.app.Dialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
@@ -27,15 +28,41 @@ import net.luisr.sylon.ui.doc.DocumentActivity;
 
 import java.util.List;
 
+/**
+ * The RecyclerView.Adapter for the docsRecView of the MainActivity.
+ *
+ * Each item represents one instance of the Document class.
+ * The UI contains a preview of the first page, the name of the Document and a button for more
+ * options. These options are changing the name of or deleting the Document instance in the database.
+ * @see MainActivity
+ * @see Document
+ */
 public class DocsRecViewAdapter extends RecyclerView.Adapter<DocsRecViewAdapter.ViewHolder> {
 
-    private List<Document> docList;
-    private Activity context;
+    /** The activity from which the adapter was created. */
+    private final Activity context;
+
+    /**
+     * A list containing all the documents to be shown in the RecyclerView.
+     * @see Document
+     */
+    private final List<Document> docList;
+
+    /**
+     * The app's database.
+     * @see AppDatabase
+     */
     private AppDatabase database;
 
-    public DocsRecViewAdapter(Activity context, List<Document> fileList) {
-        this.docList = fileList;
+    /**
+     * Constructor for the DocsRecViewAdapter.
+     * @param context the activity from which the adapter was created.
+     * @param docList a list containing all the documents to be shown in the RecyclerView.
+     */
+    public DocsRecViewAdapter(Activity context, List<Document> docList) {
+        this.database = AppDatabase.getInstance(context);
         this.context = context;
+        this.docList = docList;
         notifyDataSetChanged();
     }
 
@@ -49,11 +76,13 @@ public class DocsRecViewAdapter extends RecyclerView.Adapter<DocsRecViewAdapter.
 
     @Override
     public void onBindViewHolder(@NonNull DocsRecViewAdapter.ViewHolder holder, int position) {
-        database = AppDatabase.getInstance(context);
+        // get the document at the current position
         Document document = docList.get(position);
 
-        holder.txtFileName.setText(document.getName());
+        // show the name of the document
+        holder.txtDocName.setText(document.getName());
 
+        // show a preview of the first page in the document, if it exists
         Page firstPage = database.pageDao().getFirstPageInDocument(document.getId());
         if (firstPage != null) {
             String imgPath = firstPage.getImageUri();
@@ -62,24 +91,15 @@ public class DocsRecViewAdapter extends RecyclerView.Adapter<DocsRecViewAdapter.
             }
         }
 
+        // set an OnClickListener for the txtViewOptions
         holder.txtViewOptions.setOnClickListener(v -> {
             PopupMenu popup = new PopupMenu(context, holder.txtViewOptions);
             popup.inflate(R.menu.options_menu_document);
-            popup.setOnMenuItemClickListener(item -> {
-                int itemId = item.getItemId();
-                if (itemId == R.id.itemEdit) {
-                    itemEditCallback(document, position);
-                    return true;
-                } else if (itemId == R.id.itemDelete) {
-                    itemDeleteCallback(document, position);
-                    return true;
-                } else {
-                    return false;
-                }
-            });
+            popup.setOnMenuItemClickListener(item -> itemClickCallback(item, document, position));
             popup.show();
         });
 
+        // set an OnClickListener for the card as a whole
         holder.cardViewParent.setOnClickListener(v -> {
             Intent intent = new Intent(context.getBaseContext(), DocumentActivity.class);
             intent.putExtra(DocumentActivity.INTENT_EXTRA_DOCUMENT_ID, document.getId());
@@ -92,10 +112,37 @@ public class DocsRecViewAdapter extends RecyclerView.Adapter<DocsRecViewAdapter.
         return docList.size();
     }
 
+    /**
+     * Callback function for when an item in the txtViewOptions popup menu is clicked.
+     * @param item the item that was clicked.
+     * @param document the document at the current position.
+     * @param position the current position of the ViewHolder.
+     * @return true, if the click was consumed and the popup should be closed.
+     */
+    private boolean itemClickCallback(MenuItem item, Document document, int position) {
+        int itemId = item.getItemId();
+        if (itemId == R.id.itemEdit) {
+            itemEditCallback(document, position);
+            return true;
+        } else if (itemId == R.id.itemDelete) {
+            itemDeleteCallback(document, position);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Callback function for when the popup item to edit the document name was clicked.
+     * @param document the document at the current position.
+     * @param position the current position of the ViewHolder.
+     */
     private void itemEditCallback(Document document, int position) {
+        // get id and name of the document
         int documentId = document.getId();
         String documentName = document.getName();
 
+        // show dialog in which the user can enter a new name for the document
         Dialog dialog = new Dialog(context);
         dialog.setContentView(R.layout.dialog_edit_doc_name);
         int w = WindowManager.LayoutParams.MATCH_PARENT;
@@ -103,11 +150,14 @@ public class DocsRecViewAdapter extends RecyclerView.Adapter<DocsRecViewAdapter.
         dialog.getWindow().setLayout(w, h);
         dialog.show();
 
+        // get UI elements of dialog
         EditText edtText = dialog.findViewById(R.id.edtText);
         Button btnUpdate = dialog.findViewById(R.id.btnUpdate);
 
+        // set the text of the edtText to the current name of the document
         edtText.setText(documentName);
 
+        // set listeners for the enter key and the update button
         edtText.setOnEditorActionListener((textView, i, keyEvent) -> {
             dialog.dismiss();
             btnUpdateCallback(documentId, edtText.getText().toString().trim(), position);
@@ -119,15 +169,28 @@ public class DocsRecViewAdapter extends RecyclerView.Adapter<DocsRecViewAdapter.
         });
     }
 
+    /**
+     * Callback function for the update button of the dialog created by the itemEdit of the popup menu.
+     * @param id the ID of the document to update.
+     * @param name the new name of the document.
+     * @param position the current position of the ViewHolder.
+     */
     private void btnUpdateCallback(int id, String name, int position) {
         database.docDao().setName(id, name);
         docList.set(position, database.docDao().getById(id));
         notifyItemChanged(position);
     }
 
+    /**
+     * Callback function for when the popup item to delete the document name was clicked.
+     * @param document the document at the current position.
+     * @param position the current position of the ViewHolder.
+     */
     private void itemDeleteCallback(Document document, int position) {
+        // get the name of the document
         String documentName = document.getName();
 
+        // create dialog to show a warning to the user
         Dialog dialog = new Dialog(context);
         dialog.setContentView(R.layout.dialog_delete_doc);
         int w = WindowManager.LayoutParams.MATCH_PARENT;
@@ -135,17 +198,20 @@ public class DocsRecViewAdapter extends RecyclerView.Adapter<DocsRecViewAdapter.
         dialog.getWindow().setLayout(w, h);
         dialog.show();
 
+        // get UI elements of dialog
         TextView txtMessage = dialog.findViewById(R.id.txtMessage);
         Button btnDelete = dialog.findViewById(R.id.btnDelete);
         Button btnCancel = dialog.findViewById(R.id.btnCancel);
 
-        txtMessage.setText(context.getResources().getString(R.string.dialog_delete_file_msg_name, documentName));
+        // show warning to the user
+        txtMessage.setText(context.getResources().getString(R.string.dialog_delete_doc_msg_name, documentName));
 
+        // set onClickListeners for the cancel and delete (i.e. confirm) button
         btnCancel.setOnClickListener(dv -> dialog.dismiss());
-
         btnDelete.setOnClickListener(dv -> {
             dialog.dismiss();
 
+            // delete document from database and docList
             database.docDao().delete(document);
             docList.remove(position);
             notifyItemRemoved(position);
@@ -154,14 +220,14 @@ public class DocsRecViewAdapter extends RecyclerView.Adapter<DocsRecViewAdapter.
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
-        TextView txtFileName, txtViewOptions;
+        TextView txtDocName, txtViewOptions;
         MaterialCardView cardViewParent;
         ImageView imgViewFirstPage;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
 
-            txtFileName = itemView.findViewById(R.id.txtFileName);
+            txtDocName = itemView.findViewById(R.id.txtDocName);
             txtViewOptions = itemView.findViewById(R.id.txtViewOptions);
             cardViewParent = itemView.findViewById(R.id.cardViewParent);
             imgViewFirstPage = itemView.findViewById(R.id.imgViewFirstPage);
--- app/src/main/java/net/luisr/sylon/ui/main/MainActivity.java
+++ app/src/main/java/net/luisr/sylon/ui/main/MainActivity.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
@@ -19,33 +20,69 @@ import net.luisr.sylon.db.Document;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * The Main Activity of the class.
+ * This activity is the launcher activity and shows a list of all documents in the database.
+ * Via a button, the user can add a document to the database.
+ */
 public class MainActivity extends AppCompatActivity {
 
-    private List<Document> docList = new ArrayList<>();
-    private RecyclerView filesRecView;
-    private FloatingActionButton btnAdd;
-    private DocsRecViewAdapter adapter;
+    /**
+     * The app's database containing all documents and pages.
+     * @see AppDatabase
+     */
     private AppDatabase database;
+
+    /**
+     * A list containing all documents.
+     * The list is passed to the docsRecView.
+     * @see Document
+     */
+    private List<Document> docList = new ArrayList<>();
+
+    /** A RecyclerView showing all the documents in the docList. */
+    private RecyclerView docsRecView;
+
+    /**
+     * The RecyclerView.Adapter for the docsRecView.
+     * @see DocsRecViewAdapter
+     */
+    private DocsRecViewAdapter adapter;
+
+    /** A button to create a new document */
+    private FloatingActionButton btnAdd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        filesRecView = findViewById(R.id.docsRecView);
+        // get UI elements
+        docsRecView = findViewById(R.id.docsRecView);
         btnAdd = findViewById(R.id.btnAdd);
 
+        // get database and fill docList
         database = AppDatabase.getInstance(this);
         docList = database.docDao().getAll();
 
-        filesRecView.setLayoutManager(new LinearLayoutManager(this));
+        // set layout and adapter for docsRecView
+        docsRecView.setLayoutManager(new LinearLayoutManager(this));
         adapter = new DocsRecViewAdapter(MainActivity.this, docList);
-        filesRecView.setAdapter(adapter);
+        docsRecView.setAdapter(adapter);
 
+        // set OnClickListener for btnAdd
         btnAdd.setOnClickListener(v -> btnAddCallback());
     }
 
+    /**
+     * Callback function for the btnAdd OnClickListener.
+     *
+     * Creates a dialog in which the user has to enter the name of the document.
+     * After pressing a confirm button, the document is added to the database and shown in the
+     * docsRecView.
+     */
     private void btnAddCallback() {
+        // open dialog to enter document name
         Dialog dialog = new Dialog(this);
         dialog.setContentView(R.layout.dialog_add_doc);
         int w = WindowManager.LayoutParams.MATCH_PARENT;
@@ -53,9 +90,11 @@ public class MainActivity extends AppCompatActivity {
         dialog.getWindow().setLayout(w, h);
         dialog.show();
 
+        // get UI elements of dialog
         EditText edtText = dialog.findViewById(R.id.edtText);
         Button btnConfirm = dialog.findViewById(R.id.btnAdd);
 
+        // set listeners for the enter key and the confirm button
         edtText.setOnEditorActionListener((textView, i, keyEvent) -> {
             dialog.dismiss();
             btnConfirmCallback(edtText.getText().toString().trim());
@@ -67,14 +106,28 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    /**
+     * Callback function for the btnConfirm OnClickListener.
+     *
+     * Creates a new Document with the name passed by the user and saves it into the database as
+     * well as the docList.
+     * @param name the name of the Document.
+     */
     private void btnConfirmCallback(String name) {
+        // name should not be empty
         if (!name.equals("")) {
-            Document sf = new Document(name);
-            database.docDao().insert(sf);
+            // instantiate Document
+            Document doc = new Document(name);
 
-            docList.clear();
-            docList.addAll(database.docDao().getAll());
+            // insert doc to database
+            doc.setId((int) database.docDao().insert(doc));
+
+            // insert doc to docList and notify DocsRecViewAdapter
+            docList.add(doc);
             adapter.notifyDataSetChanged();
+        } else {
+            // show message that the doc name cannot be empty
+            Toast.makeText(this, getString(R.string.empty_doc_name_msg), Toast.LENGTH_SHORT).show();
         }
     }
 }
