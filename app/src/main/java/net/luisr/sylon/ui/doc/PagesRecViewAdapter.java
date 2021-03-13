a049989a0a12fef903b3dbb6a1fe53f54faa41e1
Andreas Strasser <strassan@kaifeck.de>
2021-03-13 14:08:28 +0100

    Closes #6: Add documentation to the ui.doc package

--- app/src/main/java/net/luisr/sylon/ui/doc/PagesRecViewAdapter.java
+++ app/src/main/java/net/luisr/sylon/ui/doc/PagesRecViewAdapter.java
@@ -12,16 +12,33 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import net.luisr.sylon.R;
+import net.luisr.sylon.db.Document;
 import net.luisr.sylon.db.Page;
+import net.luisr.sylon.ui.main.MainActivity;
 
 import java.util.List;
 
-
+/**
+ * The RecyclerView.Adapter for the pagesRecView of the DocumentActivity.
+ *
+ * Each item represents one instance of the Page class.
+ * The UI contains a preview of the page and the page number.
+ * @see DocumentActivity
+ * @see Page
+ */
 public class PagesRecViewAdapter extends RecyclerView.Adapter<PagesRecViewAdapter.ViewHolder> {
 
-    private final List<Page> pageList;
+    /** The activity from which the adapter was created. */
     private Activity context;
 
+    /** A list containing all the pages to be shown in the RecyclerView. */
+    private final List<Page> pageList;
+
+    /**
+     * Constructor for the PagesRecViewAdapter.
+     * @param context the activity from which the adapter was created.
+     * @param pageList a list containing all the pages to be shown in the RecyclerView.
+     */
     public PagesRecViewAdapter(Activity context, List<Page> pageList) {
         this.pageList = pageList;
         this.context = context;
@@ -38,8 +55,13 @@ public class PagesRecViewAdapter extends RecyclerView.Adapter<PagesRecViewAdapte
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        // get the page at the current position
         Page page = pageList.get(position);
+
+        // show the page number
         holder.txtPageNumber.setText(context.getResources().getString(R.string.page_number, position, page.getId()));
+
+        // set the image preview
         String imgUri = page.getImageUri();
         if (imgUri != null) {
             holder.imgViewPage.setImageURI(Uri.parse(imgUri));
@@ -51,6 +73,7 @@ public class PagesRecViewAdapter extends RecyclerView.Adapter<PagesRecViewAdapte
         return pageList.size();
     }
 
+    /** The ViewHolder class extracts the UI elements from the layout. */
     public static class ViewHolder extends RecyclerView.ViewHolder {
         ImageView imgViewPage;
         TextView txtPageNumber;
