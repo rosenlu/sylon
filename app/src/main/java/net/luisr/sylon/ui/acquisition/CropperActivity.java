eb09304651e6269c81d3d069a532ac1e0a41f68d
Luis Rosendahl <luisrosendahl@gmail.com>
2021-04-24 23:19:08 +0200

    Correct relative layout on top of image view, Issue #12

--- app/src/main/java/net/luisr/sylon/ui/acquisition/CropperActivity.java
+++ app/src/main/java/net/luisr/sylon/ui/acquisition/CropperActivity.java
@@ -1,7 +1,10 @@
 package net.luisr.sylon.ui.acquisition;
 
+import android.graphics.Rect;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.ViewTreeObserver;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
@@ -13,6 +16,9 @@ import net.luisr.sylon.db.AppDatabase;
 import net.luisr.sylon.db.Page;
 
 public class CropperActivity extends AppCompatActivity {
+
+    /** The tag used for logging */
+    private static final String TAG = "CropperActivity";
 
     public static final String INTENT_EXTRA_IMAGE_URI = "net.luisr.sylon.image_uri_to_crop";
 
@@ -28,10 +34,37 @@ public class CropperActivity extends AppCompatActivity {
 
         ImageView imgViewPage = findViewById(R.id.imgViewPage);
 
+        //imageUri = Uri.parse("file:///storage/emulated/0/Android/media/net.luisr.sylon/Sylon/img/4FwK8NU (1).jpg");
         imageUri = Uri.parse(getIntent().getStringExtra(INTENT_EXTRA_IMAGE_URI));
         imgViewPage.setImageURI(imageUri);
 
-        RelativeLayout layoutDonuts = findViewById(R.id.layoutDonuts);
+        imgViewPage.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+
+            public void onGlobalLayout() {
+
+
+                int height = imgViewPage.getHeight();
+                int width = imgViewPage.getWidth();
+                int x = imgViewPage.getLeft();
+                int y = imgViewPage.getTop();
+
+                Rect rt = imgViewPage.getDrawable().getBounds();
+
+                int ih=imgViewPage.getMeasuredHeight();//height of imageView
+                int iw=imgViewPage.getMeasuredWidth();//width of imageView
+                int iH=imgViewPage.getDrawable().getIntrinsicHeight();//original height of underlying image
+                int iW=imgViewPage.getDrawable().getIntrinsicWidth();//original width of underlying image
+
+
+                Log.d(TAG, "CROPPER Height "+height+", Width "+width+", X "+x+", Y "+y);
+                Log.d(TAG, "CROPPER Rect = "+rt);
+                Log.d(TAG, "CROPPER ih "+ih+", iw "+iw+", iH "+iH+", iW "+iW);
+
+                // remove listener
+                imgViewPage.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+
+            }
+        });
 
     }
 }
